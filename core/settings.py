3b807fad429470aa389ceb1fe257036aebe3e427
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-19 20:26:09 -0500

    Add postgres integration

--- core/settings.py
+++ core/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ForUnsaDB',
+        'USER': 'postgres',
+        'PASSWORD': 'admin', # should be changed later
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
