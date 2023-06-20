3b603c631f5a3e4611a7fb36b57fd5fc5192114d
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-20 09:23:15 -0500

    Register apps on admin.py

--- apps/forum/admin.py
+++ apps/forum/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Customuser, School, Section, Tag, Post, Comment, ReactPost, SavePost, Report 
 
 # Register your models here.
+
+admin.site.register(Customuser)
+admin.site.register(School)
+admin.site.register(Section)
+admin.site.register(Tag)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(ReactPost)
+admin.site.register(SavePost)
+admin.site.register(Report)
--- apps/forum/models/react_post.py
+++ apps/forum/models/react_post.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from . import Base, PostBase, Reaction
 
-class ReacPost(Base):
+class ReactPost(Base):
     post = models.ForeignKey(PostBase, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     post_reaction = models.ForeignKey(Reaction, on_delete=models.SET(Reaction.get_default_reaction))
