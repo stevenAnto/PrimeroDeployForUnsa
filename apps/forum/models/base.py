0b93987bb6784532c41f4f66e5b141192c7c95c2
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-19 22:05:03 -0500

    Add Base model

--- apps/forum/models/base.py
+++ apps/forum/models/base.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class Base(models.Model):
+    STATE_CHOICES = (
+        ('A', 'Active'),
+        ('I', 'Inactive'),
+        ('X', 'Deleted'),
+    )
+    state = models.CharField(max_length=1, choices=STATE_CHOICES, default='A')
